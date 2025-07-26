edbfb0da70347db4e75d983227c4f612daa6e400
Takao Shibata <chise.alter.pasta@gmail.com>
2025-07-26 21:58:41 +0900

    type

---
+++ dailyreport.go
@@ -0,0 +1,22 @@
+package main
+
+import "time"
+
+type DailyReport struct {
+	attendance Attendance
+	tasks      []Task
+}
+
+type Task struct {
+	project     string
+	description string
+	estimate    time.Duration
+	actual      time.Duration
+	completion  bool
+}
+
+type Attendance struct {
+	start     time.Time
+	end       time.Time
+	breakTime time.Duration
+}
